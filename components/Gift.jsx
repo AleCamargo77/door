430d95fcbe5b123c81b1878c7a64f237421bbf93
Alessandro Camargo <acamargodias@terra.com.br>
2023-02-19 20:28:11 -0300

    feat: class door

---
+++ components/Gift.jsx
@@ -0,0 +1,12 @@
+import styles from "../src/styles/gift.module.css";
+
+export default function Gift() {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+      <div className={styles.giftLink1}></div>
+      <div className={styles.giftLink2}></div>
+    </div>
+  );
+}
