2bda71a55426c426a2eb68196921b1315c5f2267
Alessandro Camargo <acamargodias@terra.com.br>
2023-02-23 09:16:54 -0300

    feat: finish project

--- src/pages/game/[doors]/[haveGift].tsx
+++ src/pages/game/[doors]/[haveGift].tsx
@@ -1,15 +1,29 @@
 import Door from "components/Door";
 import { CreateDoors, UpdateDoors } from "../../../../functions/doors";
 import { useEffect, useState } from "react";
-// import styles from "../styles/game.module.css";
 import styles from "../../../styles/game.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import DoorModel from "model/DoorModel";
+import { Http2ServerRequest } from "http2";
 
 export default function game() {
   const router = useRouter();
+
   const [doors, setDoors] = useState<DoorModel[]>([]);
+  const [valid, setValid] = useState(false);
+
+  useEffect(() => {
+    if (router.query.doors && router.query.haveGift) {
+      const doorsValid: number = +router.query.doors;
+      const withGift: number = +router.query.haveGift;
+
+      const qtdeDoorsValid = doorsValid >= 3 && doorsValid < 100;
+      const withGiftVallid = withGift >= 1 && withGift <= doorsValid;
+
+      setValid(qtdeDoorsValid && withGiftVallid);
+    }
+  }, [doors]);
 
   useEffect(() => {
     if (router.query.doors && router.query.haveGift) {
@@ -20,20 +34,25 @@ export default function game() {
   }, [router?.query]);
 
   function renderDoors() {
-    return doors.map((door, i) => {
-      return (
-        <Door
-          key={i}
-          value={door}
-          onChange={(newPort) => setDoors(UpdateDoors(doors, newPort))}
-        />
-      );
-    });
+    return (
+      valid &&
+      doors.map((door, i) => {
+        return (
+          <Door
+            key={i}
+            value={door}
+            onChange={(newPort) => setDoors(UpdateDoors(doors, newPort))}
+          />
+        );
+      })
+    );
   }
 
   return (
     <div className={styles.game}>
-      <div className={styles.doors}>{renderDoors()}</div>
+      <div className={styles.doors}>
+        {valid ? renderDoors() : <h1>Valores Inválidos</h1>}
+      </div>
       <div className={styles.buttons}>
         <Link href="/">
           <button>Reiniciar Jogo</button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import styles from "../styles/form.module.css";
 import Card from "../../components/Card";
 import Link from "next/link";
+import NumberInput from "components/NumberInput";
+import { useState } from "react";
 
 export default function Form() {
+  const [qtdDoors, setQtdDoors] = useState(3);
+  const [withGift, setWithGift] = useState(2);
+
   return (
     <div className={styles.form}>
       <div>
@@ -10,14 +15,25 @@ export default function Form() {
           <h2>Monty Hall</h2>{" "}
         </Card>
 
-        <Card></Card>
+        <Card>
+          <NumberInput
+            text="Quantidade portas?"
+            value={qtdDoors}
+            onChange={(newDoorWithGift) => setQtdDoors(newDoorWithGift)}
+          />
+        </Card>
       </div>
 
       <div>
-        <Card></Card>
-
+        <Card>
+          <NumberInput
+            text="Porta com Presente?"
+            value={withGift}
+            onChange={(newQtd) => setWithGift(newQtd)}
+          />
+        </Card>
         <Card bgcolor="#28a085">
-          <Link href={`/game/4/2`} className={styles.link}>
+          <Link href={`/game/${qtdDoors}/${withGift}`} className={styles.link}>
             <h3>Iniciar</h3>
           </Link>
         </Card>
